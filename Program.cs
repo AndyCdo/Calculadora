d8558d5a8d72e9be79ffd40c01d198b974aaa7ae
Andrea <chela355@hotmail.com>
2021-03-03 15:29:57 -0500

    se realizo la tarea

--- Program.cs
+++ Program.cs
@@ -6,7 +6,41 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+         Console.WriteLine("Operando: ");
+         int Operando1=Convert.ToInt32(Console.ReadLine());
+        
+         Console.WriteLine("Operador: ");
+         string operador=Convert.ToString(Console.ReadLine());
+
+         Console.WriteLine("Operando: ");
+         int Operando2=Convert.ToInt32(Console.ReadLine());
+
+         String suma= "+";
+         string resta= "-";
+         string multiplicacion= "*";
+         string division= "/";
+         
+        if (operador == suma)
+        {
+            int resultado= Operando1 + Operando2;
+            Console.WriteLine(Operando1 + operador + Operando2 + " =" + resultado);
+        }
+        if (operador == resta)
+        {
+            int resultado= Operando1 - Operando2;
+            Console.WriteLine(Operando1 + operador + Operando2 + " =" + resultado);
+        }
+        if (operador == multiplicacion)
+        {
+            int resultado= Operando1 * Operando2;
+            Console.WriteLine(Operando1 + operador + Operando2 + " =" + resultado);
+        }
+        if (operador == division)
+        {
+            int resultado= Operando1 / Operando2;
+            Console.WriteLine(Operando1 + operador + Operando2 + " =" + resultado);
+        }
+
         }
     }
 }
